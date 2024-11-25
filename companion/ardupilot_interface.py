1a8e52d8916740cfd9b3df9010a38245eda4e43b
usr <usr@gmail.com>
2024-11-25 13:40:59 +0200

    added rc override stuff

--- companion/ardupilot_interface.py
+++ companion/ardupilot_interface.py
@@ -3,9 +3,79 @@ from pymavlink import mavutil
 import time
 from collections import deque
 from contextlib import contextmanager
+from typing import List
+
+class RcOverride():
+    def __init__(self, rc_override_cb, frequency=200) -> None:
+        self.rc_override_cb = rc_override_cb
+        self.update_frequency = frequency
+        self.last_update_time = 0
+        self.rc_override_values = [0] * 4
+        self.stop_event = threading.Event()
+        self.override_lock = threading.Lock()
+        self.rc_override_thread = threading.Thread(target=self.rc_override_update,daemon=True)
+        self.rc_override_thread.start()
+    
+    def rc_override_update(self):
+        while not self.stop_event.is_set():
+                    # current_time = time.time()
+                    # next_update_time = self.last_update_time + (1/self.update_frequency)
+
+                    # # Sleep until the next update time
+                    # sleep_time = max(0, next_update_time - current_time)
+                    # time.sleep(sleep_time)
+
+                    # Update at the desired frequency
+                    with self.override_lock:
+                        self.rc_override_cb(*self.rc_override_values)
+                    
+                    # Update last_update_time after each iteration
+                    # self.last_update_time = time.time()
+
+    def kill(self):
+        self.stop_event.set()
+        self.rc_override_thread.join()
+
+    def _validate_rc_input(self, value):
+        """
+        Validate RC input value to ensure it's within the range 1000-2000.
+
+        :param value: RC input value
+        :return: Clipped RC input value
+        """
+        return max(1000, min(2000, int(value)))
+
+    def __call__(self, roll: int=None, pitch: int=None, throttle: int=None, yaw: int=None):
+        """
+        Send control inputs to the vehicle.
+
+        This method sends roll, pitch, throttle, and yaw values to control the vehicle.
+        
+        Args:
+            roll (int): Roll angle between -1000 and 2000 degrees.
+            pitch (int): Pitch angle between -1000 and 2000 degrees.
+            throttle (int): Throttle value between -1000 and 2000.
+            yaw (int): Yaw angle between -1000 and 2000 degrees.
+
+        Returns:
+            None
+
+        Note:
+            This method does not return any value. It only sends the input values to the vehicle.
+        """
+        with self.override_lock:
+            self.rc_override_values[0] = self._validate_rc_input(roll) if roll is not None else 0
+            self.rc_override_values[1] = self._validate_rc_input(pitch) if pitch is not None else 0
+            self.rc_override_values[2] = self._validate_rc_input(throttle) if throttle is not None else 0
+            self.rc_override_values[3] = self._validate_rc_input(yaw) if yaw is not None else 0
+
+    
+
 
 class ArduPilotInterface:
-    def __init__(self, connection_string, baudrate=115200, source_system=255):
+    NO_HEARTBEAT_TIMEOUT = 1.5
+
+    def __init__(self, connection_string, baudrate=115200, source_system=255, reconnect_on_the_fly=True):
         """
         Initialize the ArduPilot interface.
 
@@ -13,21 +83,21 @@ class ArduPilotInterface:
         :param baudrate: Baudrate for the connection (default is 115200)
         :param source_system: MAVLink system ID (default is 255)
         """
-        self.master_lock = threading.Lock()  # Lock for self.master access
+        self.threads: List[threading.Thread] = []
+        self.stop_event = threading.Event()
+
+        # --- connection sutff --- #
+        self.pixhawk_lock = threading.Lock()  # Lock for self.master access
         self.connection_string = connection_string
         self.baudrate = baudrate
         self.source_system = source_system
         self.master = None
+        self.reconnect_on_the_fly = reconnect_on_the_fly
+        self.connected = False
         self.connect()
 
-
-        self.stop_event = threading.Event()
-        self.ack_thread = None
-
-        self.no_heartbeat_timeout = 1
-        self.check_connection_thread = None
+        self.time_for_connection_failed = 0
         self.connected_lock = threading.Lock()  # Lock for self.master access
-        self.connected = False
 
         self.start_threads()
         # Start the acknowledgment handling thread
@@ -48,27 +118,35 @@ class ArduPilotInterface:
         # self.hud_lock = threading.Lock()
         # self.hud_thread = None
 
-
-    def connect(self):
+    def connect(self, blocking=True):
         """
         Establish a connection to the vehicle.
         """
-        with self.master_lock:
-            self.master = mavutil.mavlink_connection(
-                self.connection_string,
-                baud=self.baudrate,
-                source_system=self.source_system
-            )
+        with self.pixhawk_lock:
+            while blocking and not self.connected:
+                try:
+                    self.master = mavutil.mavlink_connection(
+                        self.connection_string,
+                        baud=self.baudrate,
+                        source_system=self.source_system
+                    )
 
-            self.master.wait_heartbeat()
-            print("connected to pixhawk")
+                    self.master.wait_heartbeat()
+                    self.connected = True
+                    print("connected to pixhawk")
+                except Exception as e:
+                    print("could not connect on the fly. retrying...")
+                    time.sleep(1)
 
     def start_threads(self):
         self.ack_thread = threading.Thread(target=self.recv_match_handler, daemon=True)
         self.ack_thread.start()
+        self.threads.append(self.ack_thread)
 
-        self.check_connection_thread = threading.Thread(target=self.update_connection_state, daemon=True)
+        self.check_connection_thread = threading.Thread(target=self.connection_handler, daemon=True)
         self.check_connection_thread.start()
+        self.threads.append(self.check_connection_thread)
+
         # Start the RC override sending thread
         # self.rc_override_thread = threading.Thread(target=self.send_rc_override, daemon=True)
         # self.rc_override_thread.start()
@@ -77,14 +155,48 @@ class ArduPilotInterface:
         # self.hud_thread = threading.Thread(target=self.update_hud_data, daemon=True)
         # self.hud_thread.start()
 
-    def update_connection_state(self):
-        with self.connected_lock:
-            if time.time() - self.last_heartbeat_time > self.no_heartbeat_timeout:
-                self.connected = False
-            else:
-                self.connected = True
+    def recv_match_handler(self):
+        """
+        Thread function to handle incoming acknowledgments.
+        """
+        while not self.stop_event.is_set():
+            try:
+                with self.pixhawk_lock:
+                    msg = self.master.recv_match(blocking=True, timeout=1)
+                    # print(f"recived msg: {msg}")
+                    assert msg is not None
+            except Exception as e:
+                continue
 
-    def send_command(self, command, params, target_system=None, target_component=None):
+            msg_type = msg.get_type()
+            if msg_type == 'HEARTBEAT':
+                with self.connected_lock:
+                    self.time_for_connection_failed = time.time() + self.NO_HEARTBEAT_TIMEOUT
+
+            if msg_type == 'COMMAND_ACK':
+                with self.lock:
+                    if msg.command in self.pending_commands:
+                        self.acknowledged_commands[msg.command] = msg.result
+                        self.pending_commands.remove(msg.command)
+                        print(f"Command {msg.command} acknowledged with result {msg.result}.")
+
+            elif msg_type == 'PARAM_VALUE':
+                # Handle parameter acknowledgments if necessary
+                pass  # We'll handle parameter acknowledgments in set_parameter method
+            # print(msg)
+
+    def connection_handler(self):
+        while not self.stop_event.is_set():
+            time.sleep(self.NO_HEARTBEAT_TIMEOUT / 3)
+            with self.connected_lock:
+                if time.time() < self.time_for_connection_failed:
+                    self.connected = True
+                else:
+                    self.connected = False
+                    if self.reconnect_on_the_fly:
+                        self.connect(blocking=True)
+
+    def send_command(self, command, params):
         """
         Send a command_long message to the vehicle without blocking for acknowledgment.
 
@@ -93,16 +205,14 @@ class ArduPilotInterface:
         :param target_system: Target system ID (default is the connected vehicle)
         :param target_component: Target component ID (default is the connected component)
         """
-        if target_system is None:
-            target_system = self.master.target_system
-        if target_component is None:
-            target_component = self.master.target_component
+        target_system = self.master.target_system
+        target_component = self.master.target_component
 
         # Ensure params list has exactly 7 elements
         params += [0.0] * (7 - len(params))
 
         # Send the command_long message
-        with self.master_lock:
+        with self.pixhawk_lock:
             self.master.mav.command_long_send(
                 target_system,
                 target_component,
@@ -116,31 +226,6 @@ class ArduPilotInterface:
             self.pending_commands.append(command)
             self.acknowledged_commands[command] = None  # None indicates pending
 
-    def recv_match_handler(self):
-        """
-        Thread function to handle incoming acknowledgments.
-        """
-        while not self.stop_event.is_set():
-            try:
-                with self.master_lock:
-                    msg = self.master.recv_match(blocking=True, timeout=1)
-                    print(f"recived msg: {msg}")
-                if msg:
-                    msg_type = msg.get_type()
-                    if msg_type == 'HEARTBEAT':
-                        self.last_heartbeat_time = time.time()
-                    if msg_type == 'COMMAND_ACK':
-                        with self.lock:
-                            if msg.command in self.pending_commands:
-                                self.acknowledged_commands[msg.command] = msg.result
-                                self.pending_commands.remove(msg.command)
-                                print(f"Command {msg.command} acknowledged with result {msg.result}.")
-                    elif msg_type == 'PARAM_VALUE':
-                        # Handle parameter acknowledgments if necessary
-                        pass  # We'll handle parameter acknowledgments in set_parameter method
-            except Exception as e:
-                print(f"Error in acknowledgment thread: {e}")
-                continue
 
     def get_command_status(self, command):
         """
@@ -152,23 +237,12 @@ class ArduPilotInterface:
         with self.lock:
             return self.acknowledged_commands.get(command)
 
-    def stop(self):
-        """
-        Stop the acknowledgment, RC override, and HUD threads.
-        """
-        self.stop_event.set()
-        if self.ack_thread and self.ack_thread.is_alive():
-            self.ack_thread.join()
-        if self.rc_override_thread and self.rc_override_thread.is_alive():
-            self.rc_override_thread.join()
-        if self.hud_thread and self.hud_thread.is_alive():
-            self.hud_thread.join()
 
     def arm(self):
         """
         Arm the vehicle without blocking for acknowledgment.
         """
-        with self.master_lock:
+        with self.pixhawk_lock:
             if self.master.motors_armed():
                 print("Vehicle is already armed.")
                 return
@@ -183,7 +257,7 @@ class ArduPilotInterface:
         """
         Disarm the vehicle without blocking for acknowledgment.
         """
-        with self.master_lock:
+        with self.pixhawk_lock:
             if not self.master.motors_armed():
                 print("Vehicle is already disarmed.")
                 return
@@ -200,7 +274,7 @@ class ArduPilotInterface:
 
         :param mode: Mode string (e.g., 'STABILIZE', 'LOITER', 'AUTO')
         """
-        with self.master_lock:
+        with self.pixhawk_lock:
             mode_id = self.master.mode_mapping().get(mode)
             if mode_id is None:
                 print(f"Unknown mode: {mode}")
@@ -213,74 +287,29 @@ class ArduPilotInterface:
         )
 
     @contextmanager
-    def move_vehicle(self, roll=None, pitch=None, throttle=None, yaw=None):
-        """
-        Context manager to send RC_CHANNELS_OVERRIDE messages to control the vehicle's movement.
-        When the context is exited, it stops commanding the vehicle.
-
-        Channel Mapping:
-        - Channel 1: Roll
-        - Channel 2: Pitch
-        - Channel 3: Throttle
-        - Channel 4: Yaw
-
-        :param roll: Roll input (channel 1), range 1000-2000
-        :param pitch: Pitch input (channel 2), range 1000-2000
-        :param throttle: Throttle input (channel 3), range 1000-2000
-        :param yaw: Yaw input (channel 4), range 1000-2000
-        """
+    def rc_override(self):
         try:
-            self._start_rc_override(roll, pitch, throttle, yaw)
-            yield
+            rc_override = None
+            rc_override = RcOverride(rc_override_cb=self._send_rc_override)
+            yield rc_override
         finally:
-            self._stop_rc_override()
+            if rc_override:
+                rc_override.kill()
 
-    def _start_rc_override(self, roll=None, pitch=None, throttle=None, yaw=None):
-        """
-        Start sending RC override commands.
 
-        :param roll: Roll input (channel 1), range 1000-2000
-        :param pitch: Pitch input (channel 2), range 1000-2000
-        :param throttle: Throttle input (channel 3), range 1000-2000
-        :param yaw: Yaw input (channel 4), range 1000-2000
-        """
-        with self.rc_override_lock:
-            # Update the RC override values for the specified channels
-            self.rc_override_values[0] = self._validate_rc_input(roll) if roll is not None else 0
-            self.rc_override_values[1] = self._validate_rc_input(pitch) if pitch is not None else 0
-            self.rc_override_values[2] = self._validate_rc_input(throttle) if throttle is not None else 0
-            self.rc_override_values[3] = self._validate_rc_input(yaw) if yaw is not None else 0
-
-    def _stop_rc_override(self):
-        """
-        Stop sending RC override commands by resetting the override values.
-        """
-        with self.rc_override_lock:
-            self.rc_override_values = [0] * 8  # Reset all channels to 0 (no override)
-
-    def send_rc_override(self):
+    def _send_rc_override(self, roll, pitch, throttle, yaw):
         """
         Thread function to send RC_CHANNELS_OVERRIDE messages at regular intervals.
         """
-        while not self.stop_event.is_set():
-            with self.rc_override_lock:
-                # Send the RC_CHANNELS_OVERRIDE message
-                with self.master_lock:
-                    self.master.mav.rc_channels_override_send(
-                        self.master.target_system,
-                        self.master.target_component,
-                        *self.rc_override_values
-                    )
-            time.sleep(0.1)  # Send at 10 Hz
+        rc_override18 = [roll, pitch, throttle, yaw] + [0] * 14
+        with self.pixhawk_lock: #TODO make sure lock is nedded
+            print('sent rc override')
+            self.master.mav.rc_channels_override_send(
+                self.master.target_system,
+                self.master.target_component,
+                *rc_override18
+                )
 
-    def _validate_rc_input(self, value):
-        """
-        Validate RC input value to ensure it's within the range 1000-2000.
-
-        :param value: RC input value
-        :return: Clipped RC input value
-        """
-        return max(1000, min(2000, int(value)))
 
     def set_parameter(self, param_name, param_value, param_type=None, timeout=10):
         """
@@ -302,7 +331,7 @@ class ArduPilotInterface:
                 raise ValueError("Unsupported parameter value type")
 
         # Send PARAM_SET message
-        with self.master_lock:
+        with self.pixhawk_lock:
             self.master.mav.param_set_send(
                 self.master.target_system,
                 self.master.target_component,
@@ -314,7 +343,7 @@ class ArduPilotInterface:
         # Wait for PARAM_VALUE message with matching param_name
         start_time = time.time()
         while time.time() - start_time < timeout:
-            with self.master_lock:
+            with self.pixhawk_lock:
                 msg = self.master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
             if msg:
                 if msg.param_id.decode('utf-8').strip('\x00') == param_name:
@@ -343,7 +372,7 @@ class ArduPilotInterface:
         """
         while not self.stop_event.is_set():
             try:
-                with self.master_lock:
+                with self.pixhawk_lock:
                     msg = self.master.recv_match(type='VFR_HUD', blocking=True, timeout=1)
                 if msg:
                     with self.hud_lock:
@@ -359,3 +388,13 @@ class ArduPilotInterface:
             except Exception as e:
                 print(f"Error in HUD thread: {e}")
                 continue
+    
+
+    def stop(self):
+        """
+        Stop the acknowledgment, RC override, and HUD threads.
+        """
+        self.stop_event.set()
+        for thread in self.threads:
+            if thread and thread.is_alive():
+                thread.join()
--- companion/main.py
+++ companion/main.py
@@ -4,6 +4,12 @@ from ardupilot_interface import ArduPilotInterface
 def main():
     pixhawk = ArduPilotInterface('/dev/ttyACM0')
     while True:
+        print(f"ardupilot is {'connected' if pixhawk.connected else 'disconnected'}")
+        with pixhawk.rc_override() as rc_override:
+            roll = 1400
+            pitch = 1600
+            while True:
+                rc_override(roll=roll, pitch=pitch)
         time.sleep(1)
 
 
